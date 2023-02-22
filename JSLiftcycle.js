9e6dc44a8c31ff643e399b12be9e773316373ff1
Mohammad Junidy <mohmmedjunide73@gmail.com>
2023-02-22 16:09:26 +0200

    first commit

---
+++ JSLiftcycle.js
@@ -0,0 +1,8 @@
+//Call stack is a datastructer that records where in the program we are.
+//Push Pop to and from it
+//What happens when things are slow?
+//Blocking happens, when you make calls that are sycronhes it will block the stack untill they are finished
+//thus makes the UI unavailable to do anything or runs any other code
+//to solve this we use asynchronous callbacks
+//how does a settimeout work, how does it push thigs onto the stack after sometime?
+//Concurrency & the Event Loop
